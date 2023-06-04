27fc0e214f3e93dd51030697609225a6d81e06bb
Kaffff <kazuki.takahashi@techouse.jp>
2023-06-05 00:14:40 +0900

    port を変更

--- todo/vite.config.ts
+++ todo/vite.config.ts
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
-    host: "0.0.0.0",
+    port: 13001,
+    host: '0.0.0.0',
   },
   plugins: [react()],
 });
