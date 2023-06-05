3ce8f8edaddce6a4ee508adf3b346c42bc5971c6
ReiFujise00 <108464619+ReiFujise00@users.noreply.github.com>
2023-06-05 20:57:48 +0900

    Make simple todo list (#1)
    * タスクの追加をできるように
    
    * タスクの削除ができるようにする

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export const App = () => {
-  return <div>App</div>;
+  const form = useForm();
+  const [todoItemList, setTodoItemList] = useState<string[]>([]);
+
+  const handleClick = form.handleSubmit(({ todoItem }) => {
+    setTodoItemList((currentItemList) => [...currentItemList, todoItem]);
+    form.resetField('todoItem');
+  });
+
+  const handleDeleteItem = (todoItem: string) => {
+    const removedList = todoItemList.filter((item) => item !== todoItem)
+    setTodoItemList(removedList);
+  };
+  return (
+    <div>
+      <input {...form.register('todoItem')} ></input>
+      <button onClick={handleClick}>追加</button>
+      <ol>
+        {
+          todoItemList.map((todoItem)=> (
+          <li key = {todoItem}>{todoItem}
+          <button onClick = {() => handleDeleteItem(todoItem)}>削除</button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
