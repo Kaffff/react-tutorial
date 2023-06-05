4554121171f4558886aab1d7c6783653a7cd6f20
Rei Fujise <rei.fujise@techouse.jp>
2023-06-05 20:14:46 +0900

    タスクの追加をできるように

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export const App = () => {
-  return <div>App</div>;
+  const form = useForm();
+  const [todoItemList, setTodoItemList] = useState<string[]>([]);
+
+  const handleClick = form.handleSubmit(({ todoItem }) => {
+    setTodoItemList((currentItemList) => [...currentItemList, todoItem]);
+  });
+  return (
+    <div>
+      <input {...form.register('todoItem')} ></input>
+      <button onClick={handleClick}>追加</button>
+      <ol>
+        {
+          todoItemList.map((todoItem)=><li>{todoItem}</li>)
+        }
+      </ol>
+    </div>
+  );
 };
