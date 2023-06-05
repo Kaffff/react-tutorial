dcaf26e7571d2fb0aa7bada57ac1f225b3898c94
Rei Fujise <rei.fujise@techouse.jp>
2023-06-05 20:33:00 +0900

    タスクの削除ができるようにする

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -7,15 +7,24 @@ export const App = () => {
 
   const handleClick = form.handleSubmit(({ todoItem }) => {
     setTodoItemList((currentItemList) => [...currentItemList, todoItem]);
+    form.resetField('todoItem');
   });
+
+  const handleDeleteItem = (todoItem: string) => {
+    const removedList = todoItemList.filter((item) => item !== todoItem)
+    setTodoItemList(removedList);
+  };
   return (
     <div>
       <input {...form.register('todoItem')} ></input>
       <button onClick={handleClick}>追加</button>
       <ol>
         {
-          todoItemList.map((todoItem)=><li>{todoItem}</li>)
-        }
+          todoItemList.map((todoItem)=> (
+          <li key = {todoItem}>{todoItem}
+          <button onClick = {() => handleDeleteItem(todoItem)}>削除</button>
+          </li>
+        ))}
       </ol>
     </div>
   );
